b5bc78bdb2a7d3ab95f8c2cd17b4a0e6f8f54f9c
zhangjiahui <2319671152@qq.com>
2024-10-20 22:06:03 +0800

    leecode: 提交字符串查找专题 / leecode 28题

--- 00.力扣/1-100/28.找出字符串中第一个匹配项的下标.ts
+++ 00.力扣/1-100/28.找出字符串中第一个匹配项的下标.ts
@@ -1,13 +1,24 @@
 // 2024.4.13 https://leetcode.cn/problems/find-the-index-of-the-first-occurrence-in-a-string/description/
 
-// method - 59ms
+// method1 - 59ms
 function strStr(haystack: string, needle: string): number {
   return haystack.indexOf(needle);
 }
 
-// method1 - KMP(不懂啊) -
+// method2 - 暴力解法
 function strStr1(haystack: string, needle: string): number {
-  return haystack.indexOf(needle);
+  const n = haystack.length;
+  const m = needle.length;
+
+  for (let i = 0; i <= n - m; i++) {
+    let index = i;
+    for (let j = 0; j < m; j++) {
+      if (haystack[index++] !== needle[j]) break;
+      if (j === m - 1) return i;
+    }
+  }
+
+  return -1;
 }
 
 console.log(strStr("leetcode", "leeto"));
