b63c45c53cd1a66c4c96e3e6f291bcc5dd68bc10
zhangjiahui <2319671152@qq.com>
2024-10-25 14:27:37 +0800

    feat: 435.无重叠区间

--- 00.力扣/1-100/1.两数之和.ts
+++ 00.力扣/1-100/1.两数之和.ts
@@ -5,9 +5,7 @@ function twoSum(nums: number[], target: number): number[] {
 
   for (let i = 0; i < nums.length; i++) {
     const diff = target - nums[i];
-
     if (map.has(diff)) return [map.get(diff), i];
-
     map.set(nums[i], i);
   }
 
